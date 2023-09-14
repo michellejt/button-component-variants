2e7ec2167abef92ba4c9dddb8a3416956731ec00
michellejt <mtongas@gmail.com>
2023-09-15 01:05:15 +0930

    add some more logic to map over and display button variants on the page

--- src/components/Button.js
+++ src/components/Button.js
@@ -11,15 +11,24 @@ const buttonClasses = {
     "px-4 py-2 bg-default-grey outline-none rounded text-white shadow-indigo-200 shadow-lg font-medium active:shadow-none active:scale-95 hover:bg-indigo-600 focus:bg-indigo-600 focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 disabled:bg-gray-400/80 disabled:shadow-none disabled:cursor-not-allowed transition-colors duration-200",
   secondary:
     "px-4 py-2 bg-transparent outline-none border-2 border-indigo-400 rounded text-indigo-500 font-medium active:scale-95 hover:bg-indigo-600 hover:text-white hover:border-transparent focus:bg-indigo-600 focus:text-white focus:border-transparent focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 disabled:bg-gray-400/80 disabled:shadow-none disabled:cursor-not-allowed transition-colors duration-200",
+  text: "bg-black",
 };
 
-export default function Button({ children, variant, onClick }) {
+export default function Button({ children, variant, link, onClick, state }) {
   // Use the variant prop to access the corresponding class name
   const className = buttonClasses[variant] || buttonClasses.default; // Set as default button class
   return (
-    // Use the className attribute to apply the class name to the button element
-    <button className={className} onClick={onClick}>
-      {children}
-    </button>
+    // Use a conditional operator to check the variant prop and return either a button or a link element
+    variant === "text" ? (
+      // If the variant is text, return a link element with the children as the text
+      <a href={link} className={className} onClick={onClick}>
+        {children}
+      </a>
+    ) : (
+      // Otherwise, return a button element with the children as the content
+      <button className={`${className} ${state}`} onClick={onClick}>
+        {children}
+      </button>
+    )
   );
 }
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,27 +1,82 @@
 import React from "react";
 import Button from "./Button";
 
-// Define an array of button variants
+// Define an object of button variants
+const buttonVariants = {
+  default: "default",
+  "&:hover, &:focus": "default",
+  "default-outline": "default",
+  "default-outline-&:hover, &:focus": "default",
+  text: "default",
+  "text &:hover, &:focus": "default",
+  "disable-shadow": "default",
+  disabled: "default",
+  warning: "warning",
+  primary: "primary",
+  secondary: "secondary",
+};
 
+// Define a function component that renders the buttons
 export default function Content() {
-  const buttonVariants = [
-    "default",
-    "danger",
-    "success",
-    "warning",
-    "primary",
-    "secondary",
-  ];
+  // Use the return statement inside the function
   return (
     <>
       <div className="flex-1 p-10 text-2xl font-bold">
         <h1 className="text-gray-600">Buttons</h1>
-        {/* map() method to loop over the button variants and render a Button component for each buttonVariant */}
-        {buttonVariants.map((variant) => (
-          <Button variant={variant} key={variant}>
-            {variant}
-          </Button>
-        ))}
+        {/* Use grid-cols-2 for the first 6 buttons */}
+        <div className="grid grid-cols-2 gap-4">
+          {/* Loop over the first 6 button variants */}
+          {Object.entries(buttonVariants)
+            .slice(0, 6)
+            .map(([key, value]) => (
+              <div class="justify-items-center">
+                <p className="text-xs font-extralight">{`<Button variant="${key}" />`}</p>
+                <Button
+                  state="hullo"
+                  variant={key}
+                  key={key}
+                  {...(key === "text" ? { link: "#" } : {})}
+                >
+                  {value}
+                </Button>
+              </div>
+            ))}
+        </div>
+        {/* Use grid-cols-3 for the 7th and 8th buttons */}
+        <div className="grid grid-cols-3 gap-4">
+          {/* Loop over the 7th and 8th button variants */}
+          {Object.entries(buttonVariants)
+            .slice(6, 8)
+            .map(([key, value]) => (
+              <div class="justify-items-center">
+                <Button
+                  variant={key}
+                  key={key}
+                  //Using a conditional operator this specifices shen the variant is equal to text pass the link prop, otherwise leave it empty
+                  {...(key === "text" ? { link: "#" } : {})}
+                >
+                  {value}
+                </Button>
+              </div>
+            ))}
+        </div>
+        {/* Use grid-cols-4 for the rest of the buttons */}
+        <div className="grid grid-cols-4 gap-4">
+          {/* Loop over the rest of the button variants */}
+          {Object.entries(buttonVariants)
+            .slice(8)
+            .map(([key, value]) => (
+              <div class="justify-items-center">
+                <Button
+                  variant={key}
+                  key={key}
+                  {...(key === "text" ? { link: "#" } : {})}
+                >
+                  {value}
+                </Button>
+              </div>
+            ))}
+        </div>
       </div>
     </>
   );
